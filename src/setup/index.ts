e935ff5dd539f75bedc46b2ad026650f4582b3e5
Oli <oli@olillin.com>
2026-08-01 17:37:09 +0200

    Add validation of environment variables

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -3,9 +3,55 @@ import { engine } from 'express-handlebars'
 import livereload from 'livereload'
 import connectlivereload from 'connect-livereload'
 
+const FgRed = '\x1b[31m'
+const Reset = '\x1b[0m'
+
+// Load .env file
 import dotenv from 'dotenv'
 dotenv.config()
 
+// Validate environment variables
+const requiredEnvironment = [
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'API_KEY_ID',
+    'API_KEY_TOKEN',
+    'REDIRECT_URI',
+] as const
+
+const missingEnvironment = requiredEnvironment.filter(
+    envName =>
+        typeof process.env[envName] !== 'string' || process.env[envName] === ''
+)
+if (missingEnvironment.length > 0) {
+    console.error(
+        FgRed +
+            '\n\n' +
+            'Detected missing environment variables. Did you create a ' +
+            'Gamma client? Follow the guide at ' +
+            'https://gamma-docs.olillin.com/website/#creating-a-user-client ' +
+            'and fill in the credentials in the .env file.\n\n' +
+            'Missing environment variables:\n' +
+            `  ${missingEnvironment.join('\n  ')}` +
+            '\n\n' +
+            Reset
+    )
+
+    process.exit(1)
+}
+
+// Declare process.env type
+type RequiredEnvironment = {
+    [k in (typeof requiredEnvironment)[number]]: string
+}
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv extends RequiredEnvironment {}
+    }
+}
+
+// Configure directories
 const viewsDir = './src'
 const publicDir = './src/public'
 
